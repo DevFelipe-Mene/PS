461508e8fb8a4eb1a2fe0dcbda28b02debd114a4
felipe-dev <felpz947@hotmail.com>
2021-11-18 13:22:14 -0300

    PS finalizada

--- ps.ino
+++ ps.ino
@@ -2,48 +2,66 @@
 #include <ArduinoJson.h>
 
 //Entradas
-const int termistor = A2;
+const int termistor = A1;
 const int turbPin = A0;
-const int phPin = A1;
+const int phPin = A2;
 
 //Saidas
-const int led = 13;
+const int led = 12;
 
 //Variaveis
-float turbidez, tc, vO, r2, logR2, t, c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07, r1 = 10000;    
-int temperatura, ph;
+float tc, vO, r2, logR2, t, c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07, r1 = 10000;    
+int  turbidez, ph;
+float temperatura;
+
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(termistor, INPUT);
   pinMode(turbPin, INPUT);
   pinMode(phPin, INPUT);
   pinMode(led, OUTPUT);
 }
+
+
+
 void loop() {
-    StaticJsonDocument<100> json; 
-    
+   
     temperatura = readTemp(termistor);
     turbidez = analogRead(turbPin);
     ph = analogRead(phPin);
-    //ph = ph * 2;
-    
-    Serial.print("Temperatura: ");
+    ph = ph * 2;
+  
+    /*Serial.print("Temperatura: ");
     Serial.println(temperatura);
     Serial.print("Turbidez: ");
     Serial.println(turbidez);
     Serial.print("PH: ");
-    Serial.println(ph);
+    Serial.println(ph);*/
+    boolean status;
+    char dado;
+    if(Serial.available() > 0){
     
+        dado = Serial.read();
+
+        if(dado == 'A'){
+            digitalWrite(led, HIGH); 
+            status = true;
+            delay(3000);  
+        }
+    } 
+    
+    StaticJsonDocument<100> json;
     json["Temperatura"] = temperatura;
     json["Turbidez"] = turbidez;
     json["PH"] = ph;
-    
+    //json["Status"]= status;
     serializeJson(json, Serial);
     
     Serial.println();
     
-    delay(10000);
+    delay(30000);
+  
 }
 
 float readTemp(int termistor) {
@@ -52,5 +70,6 @@ float readTemp(int termistor) {
   logR2 = log(r2);
   t = (1.0 / (c1 + c2 * logR2 + c3 * logR2 * logR2 * logR2));
   tc = t - 273.15;
+
   return tc;
 }
